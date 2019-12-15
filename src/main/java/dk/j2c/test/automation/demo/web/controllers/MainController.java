7212ef7410c8c606be818850905997caa9ed259c
Johan Pramming <johanpramming@outlook.com>
2019-12-15 23:31:50 +0100

    initial import

---
+++ src/main/java/dk/j2c/test/automation/demo/web/controllers/MainController.java
@@ -0,0 +1,14 @@
+package dk.j2c.test.automation.demo.web.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String mainPage() {
+        return "{ \"message\": \"Demo app landing page\" }";
+    }
+}
---
+++ src/test/java/dk/j2c/test/automation/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.j2c.test.automation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
